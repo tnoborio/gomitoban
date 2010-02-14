cf1bf4454b32ff718da540ca4c1a0d85ad50c1e2
Tokusei Noborio <t.noborio@gmail.com>
2010-02-14 18:17:55 +0900

    Delete unecessary code.

--- src/gomitoban/servlet.clj
+++ src/gomitoban/servlet.clj
@@ -14,9 +14,6 @@
   (let [conf (load-config)]
     (setup-tw (:username conf) (:password conf))))
 
-(defn create [content author]
-  (ds/create {:kind "Greeting" :author author :content content :date (java.util.Date.)}))
-
 (defroutes bomitoban-app
   (GET "/cron/tweet"
        (html (update "test")))
@@ -26,6 +23,9 @@
 	     frnds (friends)
 	     uf (unfollowers fllws frnds)]
 	 (follow! uf)
-	 (html "ok"))))
+	 (html "ok")))
+  (GET "/cron/mentions"
+       (setup)
+       (html (mentions))))
 
 (defservice bomitoban-app)
